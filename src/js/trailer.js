d42e84226656d3c375c2e6d00b3e0dd455ebd8c2
Dmytro Kravchenko <dmitrij.kravchenko.1997@gmail.com>
2023-01-14 19:06:14 +0200

    v. 1.2.11

--- src/js/trailer.js
+++ src/js/trailer.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 import { KEY } from './api';
-import { modalBackdrop } from './refs'
+import { modalBackdrop } from './refs';
+
 const getMoviesTrailer = async key => {
   const { data } = await axios.get(`/movie/${key}/videos?api_key=${KEY}`);
-
   return data.results.filter(item => {
     if (item.site === 'YouTube') {
       return item;
-    }
+    };
   });
 };
 
@@ -16,21 +16,17 @@ export const movieTrailer = async keyId => {
   await getMoviesTrailer(keyId).then(
     r => (movie = `https://www.youtube.com/embed/${r[0].key}`))
     .catch(error => movie=false)
-    
   return movie;
- 
 };
 
 // функция на экспорт. Принимает ключ(id фильма как 'key' в 'li')
 // при запросе записывается в movie
-
 // movieTrailer(507086);
 
 export async function trailerMarkup(event)  {
-  const movieId = Number(event.target.getAttribute('key'))
-  const trailerBtn = document.querySelector('.trailerBtn')
-  trailerBtn.setAttribute('disabled', true)
-
+  const movieId = Number(event.target.getAttribute('key'));
+  const trailerBtn = document.querySelector('.trailerBtn');
+  trailerBtn.setAttribute('disabled', true);
   await movieTrailer(movieId).then(r => {
     if(r) {modalBackdrop.firstElementChild.insertAdjacentHTML('beforeend', 
       `<iframe id="ytplayer" type="text/html" width="782" height="360"
@@ -41,15 +37,12 @@ export async function trailerMarkup(event)  {
         else {modalBackdrop.firstElementChild.insertAdjacentHTML('beforeend', 
           `<div class="trailer-placeholder"></div>`)
           document.querySelector('.trailer-placeholder').scrollIntoView({block: "center", behavior: "smooth"})
-          }
-    })
-}
-
-
-
+          };
+    });
+};
 
 export function trailerBtnListener (key) {
-  const trailerBtn = document.querySelector('.trailerBtn')
-  trailerBtn.setAttribute('key', key)
-  trailerBtn.addEventListener('click', trailerMarkup)
-}
+  const trailerBtn = document.querySelector('.trailerBtn');
+  trailerBtn.setAttribute('key', key);
+  trailerBtn.addEventListener('click', trailerMarkup);
+};
