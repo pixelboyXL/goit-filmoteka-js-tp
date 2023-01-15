a467f49611b265dddaa5605ab668e7ce3056dbb5
Dmytro Kravchenko <dmitrij.kravchenko.1997@gmail.com>
2023-01-15 23:35:45 +0200

    v. 1.2.12

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,4 +1,5 @@
 import { loadLs, saveLs } from './storage';
+
 const themeBtn = document.getElementById('toggle-theme-btn');
 const sun = document.querySelector('.sun');
 const moon = document.querySelector('.moon');
@@ -10,31 +11,31 @@ if (themeValue === 'light') {
 	sun.style.visibility = 'hidden';
 } else {
 	moon.style.visibility = 'hidden';
-}
+};
 themeBtn.addEventListener('click', () => {
 	const val = loadLs('theme');
 	if (val === 'light') {
 		document.body.classList.add('dark');
 		if(location.pathname.split("/").slice(-1) != 'library.html') {
-			document.querySelector('.pagination-section').classList.add('dark')
-		}
-		document.querySelector('.modal').classList.add('dark')
+			document.querySelector('.pagination-section').classList.add('dark');
+		};
+		document.querySelector('.modal').classList.add('dark');
 		moon.style.visibility = 'hidden';
 		sun.style.visibility = 'visible';
 		saveLs('theme', 'dark');
 	} else {
 		document.body.classList.remove('dark');
 		if(location.pathname.split("/").slice(-1) != 'library.html') {
-			document.querySelector('.pagination-section').classList.remove('dark')
-		}
-		document.querySelector('.modal').classList.remove('dark')
+			document.querySelector('.pagination-section').classList.remove('dark');
+		};
+		document.querySelector('.modal').classList.remove('dark');
 		sun.style.visibility = 'hidden';
 		moon.style.visibility = 'visible';
 		saveLs('theme', 'light');
-	}
+	};
 });
 
 if(location.pathname.split("/").slice(-1) != 'library.html') {
 	if(loadLs('theme') === 'dark')
 	document.querySelector('.pagination-section').classList.add('dark')
-}
+};
