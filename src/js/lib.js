29c368c65a5a2193bb6b3cbc3d1658b21c93cb28
Dmytro Kravchenko <dmitrij.kravchenko.1997@gmail.com>
2023-01-26 19:29:23 +0200

    v. 1.2.20

--- src/js/lib.js
+++ src/js/lib.js
@@ -24,8 +24,8 @@ if (btnWatched !== null) {
     document.addEventListener('DOMContentLoaded', () => {
       libMarkup('Watched');
     });
-  }
-}
+  };
+};
 
 function funEmptyLib(libName) {
   const libIndex = loadLs(libName) ? loadLs(libName) : []; //'Queue'//'Watched'
@@ -35,14 +35,13 @@ function funEmptyLib(libName) {
   } else {
     btnQueue.classList.remove('btn-orange');
     btnWatched.classList.add('btn-orange');
-  }
-
+  };
   if (libIndex) {
     lib.classList.add('list-empty');
     const markupLibList = createEmptyLibMarkup(libName);
     lib.innerHTML = markupLibList;
-  }
-}
+  };
+};
 
 function createEmptyLibMarkup(data) {
   return `
@@ -50,7 +49,7 @@ function createEmptyLibMarkup(data) {
 			<h3 class= 'list-empty__title'>Your ${data} list is empty!</h3>
 			<p class='blink' >to add a movie to the ${data} list, click the corresponding button in the poster window</p>
 	</div>`;
-}
+};
 
 export function libMarkup(selectLs) {
   if (selectLs === 'Queue') {
@@ -59,18 +58,15 @@ export function libMarkup(selectLs) {
   } else {
     btnQueue.classList.remove('btn-orange');
     btnWatched.classList.add('btn-orange');
-  }
+  };
   lib.classList.remove('list-empty');
   lib.innerHTML = '';
-
   let sel = selectLs + 'Data';
-
   const lsList = loadLs(selectLs);
-
   if (!lsList || !lsList.length) {
     funEmptyLib(selectLs);
     return console.log('ваш список пуст!');
-  }
+  };
   let data = { results: loadLs(sel) };
   renderLibMarkup(data);
-}
+};
