09bf80d39b022268a9a22398a5d236350da7c4c7
Dmytro Kravchenko <dmitrij.kravchenko.1997@gmail.com>
2023-05-31 21:14:27 +0300

    v. 1.2.75

--- src/js/trendings.js
+++ src/js/trendings.js
@@ -1,7 +1,6 @@
 import { getTrending, IMG_BASE_URL, IMG_W400 } from './api';
 import { renderMarkup } from './renderMarkup';
 import { saveLs } from './storage';
-import { list } from './refs';
 
 getTrending(1).then((r) => {
 	renderMarkup(r),
